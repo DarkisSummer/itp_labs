f09c397759267f2ad122b0ed4bb1445fb9605abe
dark <darkissummer@yandex.ru>
2022-11-02 13:16:55 +0300

    done lab3

---
+++ lab3/src/Location.java
@@ -0,0 +1,45 @@
+/**
+ * This class represents a specific location in a 2D map.  Coordinates are
+ * integer values.
+ **/
+public class Location
+{
+    /** X coordinate of this location. **/
+    public int xCoord;
+
+    /** Y coordinate of this location. **/
+    public int yCoord;
+
+
+    /** Creates a new location with the specified integer coordinates. **/
+    public Location(int x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    /** Creates a new location with coordinates (0, 0). **/
+    public Location()
+    {
+        this(0, 0);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+        if(obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+            return false;
+        Location other = (Location) obj;
+        return xCoord == other.xCoord && yCoord == other.yCoord;
+    }
+
+    @Override
+    public int hashCode() {
+        return xCoord * 40 + yCoord;
+    }
+}
